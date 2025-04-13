f9642e4e862535321070e3adb9407beb9b0f0fc7
Sega1A <sergio.garciacuchallo@gmail.com>
2025-04-13 18:09:30 -0400

    added price atribute to Phone table.

--- server/src/database/queries.sql
+++ server/src/database/queries.sql
@@ -1,13 +1,16 @@
-INSERT INTO PHONE (MODEL, MARC, SPECS, REGISTER_DATE, STATE)
+INSERT INTO PHONE (MODEL, MARC, SPECS, REGISTER_DATE, PHOTO, STATE, PRICE)
 VALUES 
-('iPhone 13', 'Apple', '128GB, 5G, A15 Bionic, 6.1-inch display', '2023-11-15', 1),
-('Galaxy S23', 'Samsung', '256GB, 5G, Snapdragon 8 Gen 2, 6.1-inch AMOLED', '2023-12-01', 1),
-('Pixel 7', 'Google', '128GB, 5G, Google Tensor, 6.3-inch OLED', '2024-01-10', 1),
-('Xperia 1 V', 'Sony', '256GB, 4K OLED, Snapdragon 8 Gen 2', '2024-02-20', 1),
-('Moto G Stylus', 'Motorola', '128GB, Snapdragon 678, 6.8-inch display, Stylus', '2024-03-01', 1);
+('iPhone 13', 'Apple', '128GB, 5G, A15 Bionic, 6.1-inch display', '2023-11-15', NULL, 1, 999.00),
+('Galaxy S23', 'Samsung', '256GB, 5G, Snapdragon 8 Gen 2, 6.1-inch AMOLED', '2023-12-01', NULL, 1, 1199.00),
+('Pixel 7', 'Google', '128GB, 5G, Google Tensor, 6.3-inch OLED', '2024-01-10', NULL, 1, 799.00),
+('Xperia 1 V', 'Sony', '256GB, 4K OLED, Snapdragon 8 Gen 2', '2024-02-20', NULL, 1, 1299.00),
+('Moto G Stylus', 'Motorola', '128GB, Snapdragon 678, 6.8-inch display, Stylus', '2024-03-01', NULL, 1, 399.00);
 
-INSERT INTO INVENTORY (ID_PHONE) VALUES
+INSERT INTO INVENTORY (ID_PHONE)
+VALUES
 (1),
+(1),
+(2),
 (2),
 (2),
 (3),
@@ -15,12 +18,14 @@ INSERT INTO INVENTORY (ID_PHONE) VALUES
 (5),
 (5);
 
-INSERT INTO VOUCHER (DATE) VALUES 
+INSERT INTO VOUCHER (DATE)
+VALUES 
 ('2024-04-01'),
-('2024-04-03'),
+('2024-04-05'),
 ('2024-04-10');
 
-INSERT INTO PHONE_VOUCHER (ID_PHONE, ID_VOUCHER, QUANTITY) VALUES
+INSERT INTO PHONE_VOUCHER (ID_PHONE, ID_VOUCHER, PHONE_QUANTITY)
+VALUES
 (1, 1, 2),
 (2, 1, 1),
 (3, 2, 1),
--- server/src/services/phoneService.js
+++ server/src/services/phoneService.js
@@ -2,7 +2,7 @@ import { dbConnection } from "../database/connection.js";
 
 export async function getAllPhones() {
   const query =
-    "SELECT MODEL, MARC, SPECS, REGISTER_DATE, PHOTO, STATE FROM PHONE WHERE STATE = 1";
+    "SELECT MODEL, MARC, SPECS, REGISTER_DATE, STATE, PRICE, PHOTO FROM PHONE WHERE STATE = 1";
   const [phones] = await dbConnection.query(query);
   return phones;
 }
